9ab76db9a497ed38ea7617e6ff16d2340170c37c
CarolinaVlr <dianacarolinav97@gmail.com>
2023-01-30 18:31:22 -0500

    styles

--- src/assets/pages/Products.jsx
+++ src/assets/pages/Products.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
-import { Button, Form, InputGroup } from 'react-bootstrap';
+import { Button, Card, Col, Container, Dropdown, DropdownButton, Form, InputGroup, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { useNavigate, Navigate } from 'react-router-dom';
 import { filterProductForNameThunk, getProductsFilterCategory, getProductsThunk } from '../../store/slices/products.slice';
@@ -15,34 +15,62 @@ const Products = () => {
         dispatch(getProductsThunk())
         axios.get('https://e-commerce-api-v2.academlo.tech/api/v1/categories')
             .then(res => setCategory(res.data))
-        console.log(category)
+
     }, [])
-  
+
     return (
         <div>
-            <h1>hi</h1>
-            <InputGroup className="mb-3">
-                <InputGroup.Text id="basic-addon1"></InputGroup.Text>
-                <Form.Control
-                    placeholder="Product"
-                    value={searchProduct}
-                    onChange={e=>setSearchProduct(e.target.value)}
-                 
-                />
-                 <Button onClick={()=>dispatch(filterProductForNameThunk(searchProduct))} variant="success">Success</Button>
-            </InputGroup>
+            <Container>
+                <Row >
+                    <Col lg={3}>
 
-            <ul>{category.map(categor => (
-                <li key={categor.id} onClick={() => { dispatch(getProductsFilterCategory(categor.id)) }} >{categor.name}</li>
-            ))}</ul>
-            {products.map(prod => (
-                <div key={prod.id} onClick={() => navigate(`/products/${prod.id}`)}>
-                    <h2>{prod.title}</h2>
-                    <img src={prod.images?.[1].url} alt="" />
-                </div>
-            ))}
+                        <DropdownButton id="dropdown-basic-button" title="Categorías"  >
+                            {category.map(categor => (
+                                <Dropdown.Item key={categor.id} onClick={() => { dispatch(getProductsFilterCategory(categor.id)) }}>{categor.name}</Dropdown.Item>
+                            ))}
+                        </DropdownButton>
 
-        </div>
+                    </Col>
+                    <Col lg={9}>
+                        <InputGroup className="mb-3">
+                            <InputGroup.Text id="basic-addon1"></InputGroup.Text>
+                            <Form.Control
+                                placeholder="Product"
+                                value={searchProduct}
+                                onChange={e => setSearchProduct(e.target.value)}
+
+                            />
+                            <Button onClick={() => dispatch(filterProductForNameThunk(searchProduct))} variant="success">Success</Button>
+                        </InputGroup>
+                        <Row xs={1} md={2} lg={3} className="g-4">
+                            {products.map(prod => (
+                                <Col>
+
+                                    <Card style={{height:350}} key={prod.id} onClick={() => navigate(`/products/${prod.id}`)}>
+                                        <Card.Img variant="top" src={prod.images?.[0].url} style={{width:200, height:170, objectFit:'contain', margin:'auto', paddingTop:20}} />
+                                        <Card.Body>
+                                            <Card.Title>{prod.title}</Card.Title>
+                                            <Card.Text>
+
+                                            </Card.Text>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+
+                            ))}
+                        </Row>
+                    </Col>
+
+                </Row>
+                <Row>
+
+                </Row>
+
+
+
+            </Container>
+
+        </div >
     );
 };
 
