2a7a3522ce683da9c8b1c77c61d2de7006265b23
CarolinaVlr <dianacarolinav97@gmail.com>
2023-01-31 11:59:13 -0500

    purchases style

--- src/assets/pages/Login.jsx
+++ src/assets/pages/Login.jsx
@@ -6,13 +6,13 @@ import { useDispatch, useSelector } from 'react-redux';
 
 const Login = () => {
     const { register, handleSubmit } = useForm()
-  
+
     const dispatch = useDispatch()
 
     const submit = (data) => {
         axios.post('https://e-commerce-api-v2.academlo.tech/api/v1/users/login', data)
             .then(res => {
-               
+
                 localStorage.setItem('token', res.data.token)
 
             })
@@ -22,21 +22,33 @@ const Login = () => {
                 } console.log(error)
 
             })
-          
+
     }
 
     return (
 
         <div>
-            <h1>Login</h1>
-            <Form onSubmit={handleSubmit(submit)}>
+
+            <Form style={{ height: 350, margin: 'auto', width: 500,  padding: 50, 
+         }} onSubmit={handleSubmit(submit)}>
+                <h1 className='welcome'>Bienvenido! Inserta tu correo 
+                    y contraseña para continuar</h1>
+
+                    <div className='welcome_box'>
+                        <h2>Test Data</h2>
+                        <div><i className="fa-solid fa-envelope"></i><h2>john@gmail.com</h2></div>
+                        <div><i className="fa-solid fa-lock"></i><h2>john1234</h2></div>
+                    
+                    
+                    </div>
+                <Form.Text style={{}} className="text-muted">
+
+                </Form.Text>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
                     <Form.Label>Email address</Form.Label>
                     <Form.Control type="email" placeholder="Enter email"
                         {...register('email')} />
-                    <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text>
+
                 </Form.Group>
 
                 <Form.Group className="mb-3" controlId="formBasicPassword">
@@ -45,8 +57,8 @@ const Login = () => {
                         {...register('password')} />
                 </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Submit
+                <Button variant="primary" style={{width:'100%'}} type="submit">
+                   Login
                 </Button>
             </Form>
 
--- src/assets/pages/Purchases.jsx
+++ src/assets/pages/Purchases.jsx
@@ -1,18 +1,46 @@
 import React, { useEffect } from 'react';
+import { Card, Col, Container, Row } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import { getPurchases } from '../../store/slices/purchases.slice';
 
 const Purchases = () => {
     const purchase = useSelector(state => state.purchase)
     const dispacth = useDispatch()
-    useEffect(()=>{
+
+    useEffect(() => {
         dispacth(getPurchases())
-    },[])
+    }, [])
     console.log(purchase)
     return (
         <div>
-            <h1>purchases</h1>
-            
+            <Container style={{ padding: 50 }}>
+                <Row xs={1} md={2} lg={12} className="g-4">
+                    {purchase.map(purchas => (
+
+
+                        <Col lg={3} key={purchas.id} >
+                            <Card style={{ width: 200,height:400, padding: 10, margin: 'auto' }}>
+                                <Card.Img variant="top" style={{objectFit:'contain', width:180, height:150}} src={purchas.product.images[0].url} />
+                                <Card.Body>
+                                    <Card.Title>{purchas.product.title}a</Card.Title>
+                                    <Card.Text>
+                                        {purchas.createdAt.slice(0, 10)}
+
+                                    </Card.Text>
+                                    <Card.Text>
+                                        {purchas.quantity}
+                                    </Card.Text>
+                                    <Card.Text>
+                                        {`$${purchas.product.price}`}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+
+                </Row>
+            </Container>
+
         </div>
     );
 };
